9ed099d8f79ce6fb4bc928803b1614976a529e19
LucasRossetto <lucas.rossetto@hardis.fr>
2018-12-10 17:53:58 +0100

    Faire boucle foreach et tableau pour compléter le script !

--- script.php
+++ script.php
@@ -3,51 +3,48 @@
 //$path_parts_src = pathinfo('/src');
 //
 //$path_parts = pathinfo('./');
+
+$json = file_get_contents("config.json");
+$parsed_json = json_decode($json);
 $filenamesh = '/var/www/html/drushInstall/*.sh';
 $filenamebat = '/var/www/html/drushInstall/*.bat';
 
 //Ce script doit figurer dans un dossier ou se situe les différents script shell/batch a exécuter.
 
-if(file_exists('config.yml')){
+if(file_exists('./config.json')) {
 
-  if(file_exists('backupscript.json')){
+  foreach ($filenamesh as $filename) {
+    $params = $parsed_json->alias->drush;
 
     if (DIRECTORY_SEPARATOR == '/') {
 
       if (count(glob($filenamesh)) > 0) {
 
-        system( $filenamesh, $retval);
+        system($filenamesh . ' ' . $aliasdrush, $retval);
         echo $retval;
 
       }
-      else
-      {
+      else {
         echo "Le fichier .sh est manquant ou n'a pas été trouvé.";
       }
     }
+  }
 
-    if (DIRECTORY_SEPARATOR == '\\') {
+  if (DIRECTORY_SEPARATOR == '\\') {
 
-      if (file_exists($filenamebat)){
+    if (count(glob($filenamebat)) > 0) {
 
-        system($filenamebat, $retval);
-        echo $retval;
+      system($filenamebat, $retval);
+      echo $retval;
 
-      }
-      else
-      {
-        echo "Le fichier .bat est manquant ou n'a pas été trouvé.";
-      }
     }
-
-
+    else {
+      echo "Le fichier .bat est manquant ou n'a pas été trouvé.";
+    }
   }
-  else
-  {
-    echo "Le fichier backupscript.json est manquant ou n'a pas été trouvé.";
-  }
+
 }
 else
 {
-  echo "Le ficher config.yml est manquant ou n'a pas été trouvé.";
+  echo "Le ficher config.json est manquant ou n'a pas été trouvé.";
 }
--- script.sh
+++ script.sh
@@ -3,18 +3,18 @@
 #A modifier selon le projet !
 #Se mettre à la racine du projet !
 
-drush make config/drush/skeleton.make /tmp/drupal-installer && cp -r /tmp/drupal-installer/* src
+   $1 make config/drush/skeleton.make /tmp/drupal-installer && cp -r /tmp/drupal-installer/* src
    rm -rf /tmp/drupal-installer
 
 #Executer la commande ci-dessous depuis le répertoire « src » dans console (Prerequis drush 8):
    cd ./src
-   ../drush -y en mde_fields
+   $1 -y en mde_fields
 
 #Activer le « feature » pour la partie « estimate_fees »
-   ../drush -y en hpw_estimate_fees_paragraph_feature
+   $1 -y en hpw_estimate_fees_paragraph_feature
 
 #Mis à jour les « feature » manuellement
-   ../drush -y fr hpw_ckeditor_configuration_feature
-   ../drush -y fr hpw_directory_paragraph_feature
-   ../drush -y fr hpw_checklist_paragraph_feature
-   ../drush -y fr hpw_palmares_paragraph_feature
+   $1 -y fr hpw_ckeditor_configuration_feature
+   $1 -y fr hpw_directory_paragraph_feature
+   $1 -y fr hpw_checklist_paragraph_feature
+   $1 -y fr hpw_palmares_paragraph_feature
